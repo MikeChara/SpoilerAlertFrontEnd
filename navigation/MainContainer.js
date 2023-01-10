044bb0b53ef1be5f8f53ad01dc5606dab6c7a909
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-01-10 15:18:04 +0000

    icons added to navbar

--- navigation/MainContainer.js
+++ navigation/MainContainer.js
@@ -5,51 +5,56 @@ import Ionicons from "react-native-vector-icons/Ionicons";
 
 // Screens
 import HomeScreen from "./screens/HomeScreen";
-import DetailsScreen from "./screens/DetailsScreen";
-import SettingsScreen from "./screens/SettingsScreen";
+import PantryScreen from "./screens/PantryScreen";
+import AddItemScreen from "./screens/addItemScreen";
+import ProfileScreen from "./screens/ProfileScreen";
 
 //Screen names
+const pantryName = "Pantry";
+const addItemName = "Add Item";
 const homeName = "Home";
-const detailsName = "Details";
-const settingsName = "Settings";
+const profileName = "Profile";
 
 const Tab = createBottomTabNavigator();
 
 function MainContainer() {
-	return (
-		<NavigationContainer>
-			<Tab.Navigator
-				initialRouteName={homeName}
-				screenOptions={({ route }) => ({
-					tabBarIcon: ({ focused, color, size }) => {
-						let iconName;
-						let rn = route.name;
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        initialRouteName={homeName}
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            let rn = route.name;
 
-						if (rn === homeName) {
-							iconName = focused ? "home" : "home-outline";
-						} else if (rn === detailsName) {
-							iconName = focused ? "list" : "list-outline";
-						} else if (rn === settingsName) {
-							iconName = focused ? "settings" : "settings-outline";
-						}
+            if (rn === homeName) {
+              iconName = focused ? "home" : "home-outline";
+            } else if (rn === addItemName) {
+              iconName = focused ? "add-circle" : "add-circle-outline";
+            } else if (rn === pantryName) {
+              iconName = focused ? "fast-food" : "fast-food-outline";
+            } else if (rn === profileName) {
+              iconName = focused ? "person" : "person-outline";
+            }
 
-						// You can return any component that you like here!
-						return <Ionicons name={iconName} size={size} color={color} />;
-					},
-				})}
-				tabBarOptions={{
-					activeTintColor: "tomato",
-					inactiveTintColor: "grey",
-					labelStyle: { paddingBottom: 10, fontSize: 10 },
-					style: { padding: 10, height: 70 },
-				}}
-			>
-				<Tab.Screen name={homeName} component={HomeScreen} />
-				<Tab.Screen name={detailsName} component={DetailsScreen} />
-				<Tab.Screen name={settingsName} component={SettingsScreen} />
-			</Tab.Navigator>
-		</NavigationContainer>
-	);
+            // You can return any component that you like here!
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+        })}
+        tabBarOptions={{
+          activeTintColor: "tomato",
+          inactiveTintColor: "grey",
+          labelStyle: { paddingBottom: 10, fontSize: 10 },
+          style: { padding: 10, height: 70 },
+        }}
+      >
+        <Tab.Screen name={homeName} component={HomeScreen} />
+        <Tab.Screen name={addItemName} component={AddItemScreen} />
+        <Tab.Screen name={pantryName} component={PantryScreen} />
+        <Tab.Screen name={profileName} component={ProfileScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
 }
 
 export default MainContainer;
