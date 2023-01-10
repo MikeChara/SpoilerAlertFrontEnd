31d9a1c32dc9fd4065d442e41dc001c5dd85c7cf
NicholasPervez <njpervez@gmail.com>
2023-01-10 10:35:10 +0000

    test protection

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app! Fuckayooooo....no fuckayoooo!</Text>
+      <Text>Open up App.js to start working on your app! Fuckayooooo....no fuckayoooo! stop with the fuckayoos already!</Text>
       <StatusBar style="auto" />
     </View>
   );
