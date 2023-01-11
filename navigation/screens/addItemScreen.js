403826b61e0e15336abd60e39281954c8bd057a9
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-01-11 14:22:06 +0000

    add new item page started

--- navigation/screens/addItemScreen.js
+++ navigation/screens/addItemScreen.js
@@ -1,15 +1,53 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet, TextInput } from "react-native";
 
 export default function AddItemScreen({ navigation }) {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text
-        onPress={() => navigation.navigate("Home")}
-        style={{ fontSize: 26, fontWeight: "bold" }}
-      >
-        Add Item Screen
+    <View style={{ flex: 1, alignItems: "center", textAlign: "left" }}>
+      <Text style={{ fontSize: 26, fontWeight: "bold", textAlign: "left" }}>
+        {" "}
+        Item
       </Text>
+      <TextInput style={styles.input} placeholder="Enter item" />
+      <Text style={{ fontSize: 26, fontWeight: "bold", textAlign: "left" }}>
+        {" "}
+        Add Price
+      </Text>
+      <TextInput style={styles.input} placeholder="£" />
+      <Text style={{ fontSize: 26, fontWeight: "bold", textAlign: "left" }}>
+        {" "}
+        Expiry Date
+      </Text>
+      <TextInput style={styles.input} placeholder="Enter your date" />
+      <Pressable style={styles.button}>
+        <Text style={styles.text}>Add</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  button: {
+    alignItems: "center",
+    justifyContent: "center",
+    paddingVertical: 12,
+    paddingHorizontal: 80,
+    borderRadius: 20,
+    elevation: 0,
+    backgroundColor: "#5951B7",
+  },
+  text: {
+    fontSize: 16,
+    lineHeight: 21,
+    fontWeight: "bold",
+    letterSpacing: 0.25,
+    color: "white",
+  },
+  input: {
+    height: 40,
+    width: "80%",
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
+});
