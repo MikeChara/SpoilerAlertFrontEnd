60372d8e73b4384a3cfbbbe8427db1170b811b5c
Mohamed Abdi <mohamed.abdi11@hotmail.co.uk>
2023-01-13 10:29:10 +0000

    states added to add item screen

--- navigation/screens/addItemScreen.js
+++ navigation/screens/addItemScreen.js
@@ -1,8 +1,16 @@
-import * as React from "react";
+import React, { useState } from "react";
 import { View, Text, Pressable, StyleSheet, TextInput } from "react-native";
 import DatepickerSimpleUsageShowcase from "../../Components/Calendar.js";
+import data from "../../data";
 
 export default function AddItemScreen({ navigation }) {
+  const [item, setItem] = useState();
+  const [price, setPrice] = useState();
+  const [date, setDate] = React.useState(new Date());
+
+  function handleAdd() {
+    console.log(item, price, date);
+  }
   return (
     <View style={{ flex: 1, alignItems: "center", textAlign: "left" }}>
       <Text style={{ fontSize: 26, fontWeight: "bold", textAlign: "left" }}>
@@ -10,21 +18,29 @@ export default function AddItemScreen({ navigation }) {
         Item
       </Text>
 
-      <TextInput style={styles.input} placeholder="Enter item" />
+      <TextInput
+        style={styles.input}
+        placeholder="Enter item"
+        onChangeText={(text) => setItem(text)}
+      />
 
       <Text style={{ fontSize: 26, fontWeight: "bold", textAlign: "left" }}>
         {" "}
         Expiry Date
       </Text>
-      <DatepickerSimpleUsageShowcase />
+      <DatepickerSimpleUsageShowcase setDate={setDate} date={date} />
 
       <Text style={{ fontSize: 26, fontWeight: "bold", textAlign: "left" }}>
         {" "}
         Add Price
       </Text>
-      <TextInput style={styles.input} placeholder="£" />
+      <TextInput
+        style={styles.input}
+        placeholder="£"
+        onChangeText={(price) => setPrice(price)}
+      />
 
-      <Pressable style={styles.button}>
+      <Pressable style={styles.button} onPress={handleAdd}>
         <Text style={styles.text}>Add</Text>
       </Pressable>
     </View>
