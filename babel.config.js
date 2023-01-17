fae929a92b3ccd58a864244edb5a673363b44c44
Sam MC <sam+github@sdmjc.net>
2023-01-17 19:44:11 +0000

    fixed incorrect plugin config

--- babel.config.js
+++ babel.config.js
@@ -3,19 +3,21 @@ module.exports = function (api) {
   return {
     presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
     plugins: [
-      "module:react-native-dotenv",
-      {
-        envName: "APP_ENV",
-        moduleName: "@env",
-        path: ".env",
-        blocklist: null,
-        allowlist: null,
-        blacklist: null, // DEPRECATED
-        whitelist: null, // DEPRECATED
-        safe: true,
-        allowUndefined: true,
-        verbose: false,
-      },
+      [
+        "module:react-native-dotenv",
+        {
+          envName: "APP_ENV",
+          moduleName: "@env",
+          path: ".env",
+          blocklist: null,
+          allowlist: null,
+          blacklist: null, // DEPRECATED
+          whitelist: null, // DEPRECATED
+          safe: true,
+          allowUndefined: true,
+          verbose: false,
+        },
+      ],
     ],
   };
 };
