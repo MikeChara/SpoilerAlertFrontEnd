4670c41374123b7db94106eeb58dcf28a0a196dc
Sam MC <sam+github@sdmjc.net>
2023-01-17 19:50:36 +0000

    remove unnecessary options overrides for plugin

--- babel.config.js
+++ babel.config.js
@@ -2,22 +2,6 @@ module.exports = function (api) {
   api.cache(true);
   return {
     presets: ["babel-preset-expo", "module:metro-react-native-babel-preset"],
-    plugins: [
-      [
-        "module:react-native-dotenv",
-        {
-          envName: "APP_ENV",
-          moduleName: "@env",
-          path: ".env",
-          blocklist: null,
-          allowlist: null,
-          blacklist: null, // DEPRECATED
-          whitelist: null, // DEPRECATED
-          safe: true,
-          allowUndefined: true,
-          verbose: false,
-        },
-      ],
-    ],
+    plugins: [["module:react-native-dotenv"]],
   };
 };
