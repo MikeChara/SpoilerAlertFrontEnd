8ebfd2279134d47fdbef6cdb23413a97e727d7c7
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-01-20 17:10:46 +0000

    profile page with banner

--- screens/PantryScreen.js
+++ screens/PantryScreen.js
@@ -1,43 +1,51 @@
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Image } from "react-native";
 import React from "react";
 import FoodList from "../Components/FoodList";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
 import { auth } from "../firebase-config";
 
-export default function Pantry({ setFoodList, foodList, styles, setAllStats, setWeekStats }) {
-  //this is a mutable change without using setState - seems dodgy no?
-  let todaysDate = new Date();
-  return (
-    <View style={styles.pagestyle}>
-      <View style={styles.displaybox}>
-        <View>
-          <Text style={styles.subtitle}> Total: ({foodList.length} items)</Text>
-          <FlatList
-            data={foodList}
-            renderItem={({ item, index }) => {
-              //calculates days left until expiry
-              let expiryDate = new Date(item.expires_on);
-              let msleft = expiryDate.getTime() - todaysDate.getTime();
-              let daysLeft = msleft / 1000 / 60 / 60 / 24;
-              let daysLeftRounded = Math.floor(daysLeft);
-              return (
-                <GestureHandlerRootView>
-                  <FoodList
-                    name={item.name}
-                    expiry={daysLeftRounded}
-                    foodList={foodList}
-                    setFoodList={setFoodList}
-                    id={item.id}
-                    index={index}
-                    setAllStats={setAllStats}
-                    setWeekStats={setWeekStats}
-                  />
-                </GestureHandlerRootView>
-              );
-            }}
-          />
-        </View>
-      </View>
-    </View>
-  );
+export default function Pantry({
+	setFoodList,
+	foodList,
+	styles,
+	setAllStats,
+	setWeekStats,
+}) {
+	//this is a mutable change without using setState - seems dodgy no?
+	let todaysDate = new Date();
+	return (
+		<View style={styles.pagestyle}>
+			<Image
+				source={require("../assets/foodiconbg.png")}
+				style={{ position: "absolute", opacity: 0.1, resizemode: "cover" }}
+			/>
+			<View>
+				<Text style={styles.subtitle}> Total: ({foodList.length} items)</Text>
+				<FlatList
+					data={foodList}
+					renderItem={({ item, index }) => {
+						//calculates days left until expiry
+						let expiryDate = new Date(item.expires_on);
+						let msleft = expiryDate.getTime() - todaysDate.getTime();
+						let daysLeft = msleft / 1000 / 60 / 60 / 24;
+						let daysLeftRounded = Math.floor(daysLeft);
+						return (
+							<GestureHandlerRootView>
+								<FoodList
+									name={item.name}
+									expiry={daysLeftRounded}
+									foodList={foodList}
+									setFoodList={setFoodList}
+									id={item.id}
+									index={index}
+									setAllStats={setAllStats}
+									setWeekStats={setWeekStats}
+								/>
+							</GestureHandlerRootView>
+						);
+					}}
+				/>
+			</View>
+		</View>
+	);
 }
