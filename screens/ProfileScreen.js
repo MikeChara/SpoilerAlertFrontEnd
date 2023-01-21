1710d05f34340889c3092eb95c140e2982de9300
C0dingChris <Chris.Sanger@outlook.com>
2023-01-21 16:33:20 +0000

    Profile Screen changes mirroring figma mockup

--- screens/ProfileScreen.js
+++ screens/ProfileScreen.js
@@ -7,60 +7,64 @@ import { getUserDetails } from "../Fetches/getRequests";
 import { useState, useEffect } from "react";
 
 export default function ProfileScreen({ styles }) {
-	const [userDetails, setUserDetails] = useState([]);
-	async function HandleSignOut() {
-		await signOut(auth);
-	}
-	useEffect(() => {
-		getUserDetails(auth.currentUser.uid, setUserDetails);
-	}, []);
+  const [userDetails, setUserDetails] = useState([]);
+  async function HandleSignOut() {
+    await signOut(auth);
+  }
+  useEffect(() => {
+    getUserDetails(auth.currentUser.uid, setUserDetails);
+  }, []);
 
-	return (
-		<>
-			<View style={styles.profilepagecontainer}>
-				<View style={{ backgroundColor: "red", top: 0, margin: 0 }}>
-					<Image
-						source={require("../assets/foodiconbg.png")}
-						style={styles.profilescreenbanner}
-					/>
-				</View>
-				<Image
-					source={require("./Cranks-1.png")}
-					style={styles.profilepicture}
-				/>
-			</View>
-			<View style={{ ...styles.profilepagecontainer, top: 70 }}>
-				<Text style={{ fontSize: 26, fontWeight: "bold" }}>
-					{auth.currentUser.displayName}
-				</Text>
-				<Pressable style={styles.purplebutton}>
-					<Text style={styles.purplebuttontext}>Edit Profile</Text>
-				</Pressable>
-				<Text style={styles.subtitle}>Username</Text>
-				<Input
-					style={styles.textinput}
-					disabled={true}
-					placeholder={auth.currentUser.displayName}
-				/>
-				<Text style={styles.subtitle}>Email</Text>
-				<Input
-					style={styles.textinput}
-					disabled={true}
-					placeholder={auth.currentUser.email}
-				/>
-				<Text style={styles.subtitle}>Your House</Text>
-				<Input
-					style={styles.textinput}
-					disabled={true}
-					placeholder={userDetails[0]?.name}
-				/>
-				<Pressable
-					style={{ ...styles.purplebutton, marginTop: 8 }}
-					onPress={HandleSignOut}
-				>
-					<Text style={styles.purplebuttontext}>Sign Out</Text>
-				</Pressable>
-			</View>
-		</>
-	);
+  const pageColor = "green";
+
+  return (
+    <>
+      <Image
+        source={require("../assets/foodiconbg-light.png")}
+        style={{
+          ...styles.profilescreenbg,
+          backgroundColor: pageColor,
+          opacity: 0.5,
+        }}
+      />
+
+      <View style={styles.profilepagecontainer}>
+        <Image
+          source={require("./Cranks-1.png")}
+          style={styles.profilepicture}
+        />
+
+        <Text style={{ fontSize: 26, fontWeight: "bold" }}>
+          {auth.currentUser.displayName}
+        </Text>
+        <Pressable style={styles.editbutton}>
+          <Text style={styles.purplebuttontext}>Edit Profile</Text>
+        </Pressable>
+        <Text style={styles.subtitle}>Username</Text>
+        <Input
+          style={styles.textinput}
+          disabled={true}
+          placeholder={auth.currentUser.displayName}
+        />
+        <Text style={styles.subtitle}>Email</Text>
+        <Input
+          style={styles.textinput}
+          disabled={true}
+          placeholder={auth.currentUser.email}
+        />
+        <Text style={styles.subtitle}>Your House</Text>
+        <Input
+          style={styles.textinput}
+          disabled={true}
+          placeholder={userDetails[0]?.name}
+        />
+        <Pressable
+          style={{ ...styles.purplebutton, marginTop: 8 }}
+          onPress={HandleSignOut}
+        >
+          <Text style={styles.purplebuttontext}>Sign Out</Text>
+        </Pressable>
+      </View>
+    </>
+  );
 }
