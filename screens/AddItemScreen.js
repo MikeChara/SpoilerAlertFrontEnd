c5879097faa15a88f8c3c39670aa8f5369b4ac0e
MikeChara <mikejchara@gmail.com>
2023-01-23 13:45:10 +0000

    added £ to value input plus functionality so it doesn't go to the db

--- screens/AddItemScreen.js
+++ screens/AddItemScreen.js
@@ -16,20 +16,28 @@ import { backend_link } from "@env";
 
 //props coming from MainContainer
 export default function AddItemScreen({ foodList, setFoodList, styles }) {
-  const [item, setItem] = useState();
-  const [price, setPrice] = useState();
+  const [item, setItem] = React.useState();
+  const [price, setPrice] = React.useState();
   const [date, setDate] = React.useState(new Date());
+  const [displayPrice, setDisplayPrice] = React.useState("£");
 
   const priceRegex = /^\d+(\.\d{1,2})?$/;
 
   function addPriceButtonHandle() {
-    if (priceRegex.test(price)) {
-      addFood(price, item, date, auth.currentUser.uid);
-      ModalNavigation.navigate("Pantry");
-    }
-    if (!priceRegex.test(price)) {
+    if (!priceRegex.test(price.replace("£", ""))) {
       alert("Please enter a valid price.");
     }
+    if (priceRegex.test(price)) {
+      addFood(price, item, date, auth.currentUser.uid);
+      setPrice("");
+      setItem("");
+      setDisplayPrice("£");
+      ModalNavigation.navigate("Pantry");
+    }
+  }
+  function onChangePrice(price) {
+    setPrice(price);
+    setDisplayPrice("£" + price);
   }
 
   async function addFood(price, item, date, uid) {
@@ -65,6 +73,7 @@ export default function AddItemScreen({ foodList, setFoodList, styles }) {
           style={styles.textinput}
           placeholder="Enter item"
           onChangeText={(text) => setItem(text)}
+          value={item}
         />
 
         <Text style={styles.addpagetext}> Expiry Date</Text>
@@ -80,7 +89,8 @@ export default function AddItemScreen({ foodList, setFoodList, styles }) {
           style={styles.textinput}
           placeholder="£"
           keyboardType={"decimal-pad"}
-          onChangeText={(price) => setPrice(price)}
+          onChangeText={(price) => onChangePrice(price.replace("£", ""))}
+          value={displayPrice}
         />
         <Pressable style={styles.purplebutton} onPress={addPriceButtonHandle}>
           <Text style={styles.purplebuttontext}>Add Item to Pantry</Text>
