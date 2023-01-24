f7a7b2dcffd1ac89ef7fe7e5408c764f0f9c7cdb
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-01-24 13:48:13 +0000

    background cover added to home page

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,5 +1,5 @@
 import * as React from "react";
-import { View, Text } from "react-native";
+import { View, Text, Image } from "react-native";
 import { auth } from "../firebase-config";
 import { backend_link } from "@env";
 import { getAllStats, getWeekStats } from "../Fetches/getRequests.js";
@@ -18,39 +18,63 @@ export default function HomeScreen({
   }, [foodList]);
 
   return (
-    <View style={styles.dashboardContainer}>
-      <Text style={styles.dashboardWelcome}>
-        Hi {auth.currentUser.displayName}!
-      </Text>
-      <Text style={styles.dashboardProgresstitle}>Your progress so far...</Text>
-      <View style={styles.dashboardrowContainer}>
-        <View style={{ ...styles.dashboardSquare, backgroundColor: "#0E7835" }}>
-          <Text style={styles.dashboardSubtitle}>Spent Well</Text>
-          <Text style={styles.dashboardPrice}>£32</Text>
-          <Text style={styles.dashboardText}>Last week</Text>
+    <>
+      <Image
+        source={require("../assets/foodiconbg-light.png")}
+        style={styles.backgroundcover}
+      />
+      <View style={styles.dashboardContainer}>
+        <Text style={styles.dashboardWelcome}>
+          Hi {auth.currentUser.displayName}!
+        </Text>
+        <Text style={styles.dashboardProgresstitle}>
+          Your progress so far...
+        </Text>
+        <View style={styles.dashboardrowContainer}>
+          <View
+            style={{ ...styles.dashboardSquare, backgroundColor: "lightblue" }}
+          >
+            <Text style={styles.dashboardSubtitle}>Bar Cool Thing Here</Text>
+          </View>
         </View>
-        <View style={{ ...styles.dashboardSquare, backgroundColor: "#E0358A" }}>
-          <Text style={styles.dashboardSubtitle}>Spent Well</Text>
-          <Text style={styles.dashboardPrice}>£264</Text>
-          <Text style={styles.dashboardText}>All time</Text>
+
+        <View style={styles.dashboardrowContainer}>
+          <View
+            style={{ ...styles.dashboardSquare, backgroundColor: "#0E7835" }}
+          >
+            <Text style={styles.dashboardSubtitle}>Spent Well</Text>
+            <Text style={styles.dashboardPrice}>£32</Text>
+            <Text style={styles.dashboardText}>Last week</Text>
+          </View>
+          <View
+            style={{ ...styles.dashboardSquare, backgroundColor: "#E0358A" }}
+          >
+            <Text style={styles.dashboardSubtitle}>Spent Well</Text>
+            <Text style={styles.dashboardPrice}>£264</Text>
+            <Text style={styles.dashboardText}>All time</Text>
+          </View>
+        </View>
+        <View style={styles.dashboardrowContainer}>
+          <View
+            style={{ ...styles.dashboardSquare, backgroundColor: "#FEA62A" }}
+          >
+            <Text style={styles.dashboardSubtitle}>Items Spoiled</Text>
+            <Text style={styles.dashboardPrice}>
+              {weekStats?.eatenPercentage}%
+            </Text>
+            <Text style={styles.dashboardText}>Last week</Text>
+          </View>
+          <View
+            style={{ ...styles.dashboardSquare, backgroundColor: "#DE4D28" }}
+          >
+            <Text style={styles.dashboardSubtitle}>Items Spoiled</Text>
+            <Text style={styles.dashboardPrice}>
+              {weekStats?.wastedPercentage}%
+            </Text>
+            <Text style={styles.dashboardText}>All time</Text>
+          </View>
         </View>
       </View>
-      <View style={styles.dashboardrowContainer}>
-        <View style={{ ...styles.dashboardSquare, backgroundColor: "#ED914D" }}>
-          <Text style={styles.dashboardSubtitle}>Items Spoiled</Text>
-          <Text style={styles.dashboardPrice}>
-            {weekStats?.eatenPercentage}%
-          </Text>
-          <Text style={styles.dashboardText}>Last week</Text>
-        </View>
-        <View style={{ ...styles.dashboardSquare, backgroundColor: "#DE4D28" }}>
-          <Text style={styles.dashboardSubtitle}>Items Spoiled</Text>
-          <Text style={styles.dashboardPrice}>
-            {weekStats?.wastedPercentage}%
-          </Text>
-          <Text style={styles.dashboardText}>All time</Text>
-        </View>
-      </View>
-    </View>
+    </>
   );
 }
