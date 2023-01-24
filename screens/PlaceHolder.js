cfb42f93f36cc289001128259a131141bb58ac80
C0dingChris <Chris.Sanger@outlook.com>
2023-01-24 09:15:23 +0000

    Adding loading page on social page and branding on welcome screen

--- screens/PlaceHolder.js
+++ screens/PlaceHolder.js
@@ -1,12 +1,19 @@
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, Image } from "react-native";
 import React from "react";
 
 export default function PlaceHolder() {
   return (
-    <View>
-      <Text>
-        PlaceHolder page. Here we'll link other users, such as those who are in
-        the same household or friend group
+    <View style={{ backgroundColor: "white" }}>
+      <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 26 }}>
+        Coming Soon
+      </Text>
+      <Image
+        source={require("./Animation.gif")}
+        style={{ width: 400, height: 400, opacity: 0.3, alignSelf: "center" }}
+      />
+      <Text style={{ alignSelf: "center", fontSize: 16, textAlign: "center" }}>
+        This page is still under construction, Come back later for Social
+        features
       </Text>
     </View>
   );
--- screens/WelcomeScreen.js
+++ screens/WelcomeScreen.js
@@ -1,4 +1,4 @@
-import { Text, View, Pressable } from "react-native";
+import { Text, View, Pressable, Image } from "react-native";
 import React from "react";
 import { useNavigation } from "@react-navigation/native";
 
@@ -6,6 +6,16 @@ export default function WelcomeScreen({ styles }) {
   const navigation = useNavigation();
   return (
     <View style={styles.wholepagecontainer}>
+      <Text style={{ alignSelf: "center", fontWeight: "bold", fontSize: 26 }}>
+        SpoilerAlert!
+      </Text>
+      <Image
+        source={require("./Animation.gif")}
+        style={{ width: 400, height: 400, opacity: 0.3, alignSelf: "center" }}
+      />
+      <Text style={{ fontSize: 16, textAlign: "center" }}>
+        Sign in or sign up to use Spoiler Alert!
+      </Text>
       <Pressable
         style={{ ...styles.purplebutton, marginBottom: 8 }}
         onPress={() => navigation.navigate("Sign In")}
