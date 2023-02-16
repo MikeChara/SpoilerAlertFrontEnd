99796d0d95f9a24d9863d066537347d81c906330
arooj-ilyas <112420017+arooj-ilyas@users.noreply.github.com>
2023-02-16 16:23:14 +0000

    new push

--- App.js
+++ App.js
@@ -5,16 +5,18 @@ import { styles } from "./styles.js";
 import * as eva from "@eva-design/eva";
 import { ApplicationProvider, Layout, Text } from "@ui-kitten/components";
 import { LogBox } from "react-native";
+import registerNNPushToken from "native-notify";
 //ignores all messages that may otherwise be passed to the device
 //they still show in the terminal
 LogBox.ignoreAllLogs();
 
 function App() {
-	return (
-		<ApplicationProvider {...eva} theme={eva.light}>
-			<RootNavigation styles={styles} />
-		</ApplicationProvider>
-	);
+  registerNNPushToken(6107, "RscfdJGUHSLru74JJd6STe");
+  return (
+    <ApplicationProvider {...eva} theme={eva.light}>
+      <RootNavigation styles={styles} />
+    </ApplicationProvider>
+  );
 }
 
 export default App;
